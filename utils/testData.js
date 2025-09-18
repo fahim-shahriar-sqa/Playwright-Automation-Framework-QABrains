6ac5d52eb286da4334ddadb70ae83254ae781b58
fahim-shahriar-sqa <work.fahimshahriar@gmail.com>
2025-09-18 10:11:28 +0600

    Utilites setup

---
+++ utils/testData.js
@@ -0,0 +1,12 @@
+module.exports = {
+  validUser: {
+    FirstName: 'Test01',
+    emailField: 'Test00012121@gmail.com',
+    Pass: 'Test@12345',
+    ConfirmPass: 'Test@12345',
+    ValidEmail: 'testshishir@gmail.com',
+    validPassword: 'testshishir@gmail.com' // set correct login password
+  },
+  RegisterSuccessPopup: 'Home',
+  loginSuccessPopup: 'Welcome, Abu.',
+};
