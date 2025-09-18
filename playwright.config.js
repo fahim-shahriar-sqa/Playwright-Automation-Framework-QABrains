34502dd034181182a0eb6c0c3602001cfb0c13f8
fahim-shahriar-sqa <work.fahimshahriar@gmail.com>
2025-09-18 09:53:58 +0600

    Project setup

---
+++ playwright.config.js
@@ -0,0 +1,28 @@
+// playwright.config.js
+const { defineConfig } = require('@playwright/test');
+const path = require('path');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  timeout: 200 * 1000, // 60 sec per test
+  retries: 0, // Retry once if test fails
+  workers: 1,
+  reporter: [
+    ['html', { outputFolder: path.resolve(__dirname, 'reports'), open: 'never' }],
+    ['json', { outputFile: path.resolve(__dirname, 'test-results/test-results.json') }],
+    ['list'],
+  ],
+  use: {
+    browserName: 'chromium',
+    headless: true,
+    screenshot: 'only-on-failure',
+    trace: 'on-first-retry',
+    baseURL: "https://www.drharveys.com/?srsltid=AfmBOoqs-ZrgLu3t4rHDbDQuE_0kbGGX4bc8mbMtYsz_T-hZS9LZLAcs",
+    navigationTimeout: 200 * 1000,
+    actionTimeout: 50000,
+    
+    video: {
+      mode: 'retain-on-failure', // record only failed tests
+    },
+  },
+});
